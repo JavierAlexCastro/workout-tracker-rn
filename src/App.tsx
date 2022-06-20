a0070b84017035b328cccad1994a20a6cb975c25
JavierAlexCastro <javieralexcastro95@gmail.com>
2022-06-19 23:49:45 -0500

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello world from React Web!</Text>
+    </View>
+  );
+}
+
+export default App;
